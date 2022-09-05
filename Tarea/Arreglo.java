02dcf76fb3d09b0821b332e1cecb3e3a2896bd21
CarmenSosaO <92408733+CarmenSosaO@users.noreply.github.com>
2022-09-04 23:36:04 -0500

    Add files via upload

---
+++ Tarea/Arreglo.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.empresa;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PRUEBAS
+ */
+public class Arreglo <T> {
+
+    int tamanio;
+    ArrayList<T> datos;
+
+    public Arreglo(int tam) {
+        this.tamanio = tam;
+        datos = new ArrayList(this.tamanio);
+        for (int i = 0; i < this.tamanio; i++) {
+            this.datos.add(null);
+        }
+    }
+
+    public T getElemento(int indice) {
+        if (comprobacion(indice)) {
+            return this.datos.get(indice);
+        } else {
+            System.out.println("Fuera del rango");
+        }
+        return null;
+    }
+
+    private boolean comprobacion(int indice) {
+        return indice >= 0 && indice < this.tamanio;
+    }
+
+    public void setElemento(int indice, T dato) {
+        if (comprobacion(indice)) {
+            this.datos.set(indice, dato);
+        } else {
+            System.out.println("Fuera del rango");
+        }
+    }
+
+    public int getLongitud() {
+        return this.tamanio;
+    }
+
+    public void limpiar(T dato) {
+        for (int i = 0; i < this.tamanio; i++) {
+            this.datos.set(i, dato);
+        }
+    }
+    
+    public String toString(){
+        String estado = "------------- "+this.tamanio+" -------------\n";
+        for (T dato : datos) {
+            if(dato != null){
+                estado += dato.toString()+"\n";
+            }
+        }
+        return estado;
+    }
+
+}
+
+
